377f9965cac30d84e0c28114fa891034656ab117
arkgnan <dedi.ananto@gmail.com>
2026-01-23 20:22:28 +0700

    add crud category api

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"crud-category/controller"
-	"encoding/json"
+	"crud-category/routes"
 	"net/http"
 )
 
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{
-			"status":  "OK",
-			"message": "API Running",
-		})
-	})
-
-	router.HandleFunc("GET /api/products", controller.GetAllProducts)
-	router.HandleFunc("GET /api/products/{id}", controller.GetProductByID)
-	router.HandleFunc("POST /api/products", controller.StoreProduct)
-	router.HandleFunc("PUT /api/products/{id}", controller.UpdateProduct)
-	router.HandleFunc("DELETE /api/products/{id}", controller.DeleteProduct)
+	router := routes.RegisterRoutes()
 
 	println("Server started on port 8080")
 	http.ListenAndServe(":8080", router)
